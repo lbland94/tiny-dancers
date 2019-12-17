27e4ad02bf396ca6586a319117629641eea0a867
Landon Bland <Landon.Bland@panerabread.com>
2019-12-17 10:17:26 -0600

    feature: add ability to cycle through images for a specific animal, as well as resizing images to be somewhat consistent

--- src/constants/animals.ts
+++ src/constants/animals.ts
@@ -1,41 +1,382 @@
 export const ANIMALS: IAnimal[] = [
-  { name: "bear", images: ["bear.gif", "bear2.gif"] },
-  { name: "cat", images: ["cat.gif", "cat2.gif"] },
-  { name: "chipmunk", images: ["chipmunk.gif", "chipmunk2.gif"] },
-  { name: "cow", images: ["cow.gif"] },
-  { name: "dog", images: ["dog.gif"] },
-  { name: "duck", images: ["duck.gif"] },
-  { name: "elephant", images: ["elephant.gif"] },
-  { name: "fox", images: ["fox.gif"] },
-  { name: "frog", images: ["frog.gif"] },
-  { name: "giraffe", images: ["giraffe.gif"] },
-  { name: "hamster", images: ["hamster.gif"] },
-  { name: "hippo", images: ["hippo.gif"] },
-  { name: "lion", images: ["lion.gif"] },
-  { name: "mouse", images: ["mouse.gif"] },
-  { name: "panda", images: ["panda.gif"] },
-  { name: "penguin", images: ["penguin.webp"] },
-  { name: "pig", images: ["pig2.webp"] },
-  { name: "rabbit", images: ["rabbit.gif"] },
-  { name: "reindeer", images: ["reindeer.gif"] },
-  { name: "shark", images: ["shark.gif"] },
-  { name: "sheep", images: ["sheep.gif"] },
-  { name: "snake", images: ["snake.gif"] },
-  { name: "squirrel", images: ["squirrel.gif"] },
-  { name: "tiger", images: ["tiger.gif"] },
-  { name: "unicorn", images: ["unicorn.gif"] },
-  { name: "whale", images: ["whale.gif"] },
-  { name: "giraffe", images: ["giraffe.gif"] },
-  { name: "giraffe", images: ["giraffe.gif"] },
-  { name: "giraffe", images: ["giraffe.gif"] },
-  { name: "giraffe", images: ["giraffe.gif"] },
-  { name: "giraffe", images: ["giraffe.gif"] },
-  { name: "giraffe", images: ["giraffe.gif"] },
-  { name: "farrah", images: ["farrah.gif"], large: true },
+  {
+    name: "bear",
+    images: [
+      {
+        path: "bear.gif",
+        style: "transform: scale(1.3);",
+      }, {
+        path: "bear2.gif",
+        style: "transform: scale(1.3);",
+      }, {
+        path: "bear3.gif",
+      }, {
+        path: "bear4.gif",
+      }, {
+        path: "bear5.gif",
+      }, {
+        path: "bear6.gif",
+      }, {
+        path: "bear7.gif",
+      },
+    ],
+  },
+  {
+    name: "cat",
+    images: [
+      {
+        path: "cat.gif",
+      }, {
+        path: "cat2.gif",
+      },
+    ],
+  },
+  {
+    name: "chipmunk",
+    images: [
+      {
+        path: "chipmunk.gif",
+      }, {
+        path: "chipmunk2.gif",
+        style: "transform: scale(1.5);",
+      },
+    ],
+  },
+  {
+    name: "cow",
+    images: [
+      {
+        path: "cow.gif",
+      },
+    ],
+  },
+  {
+    name: "dog",
+    images: [
+      {
+        path: "dog.gif",
+        style: "transform: scale(2);",
+      }, {
+        path: "dog2.gif",
+        style: "transform: scale(.7);",
+      }, {
+        path: "dog3.gif",
+        style: "transform: scale(1.3);",
+      }, {
+        path: "dog4.gif",
+      }, {
+        path: "dog5.gif",
+        style: "transform: scale(1.3);",
+      }, 
+    ],
+  },
+  {
+    name: "duck",
+    images: [
+      {
+        path: "duck.gif",
+      }, {
+        path: "duck2.gif",
+      },
+    ],
+  },
+  {
+    name: "elephant",
+    images: [
+      {
+        path: "elephant.gif",
+      }, {
+        path: "elephant3.gif",
+      },
+    ],
+  },
+  {
+    name: "fox",
+    images: [
+      {
+        path: "fox.gif",
+        style: "transform: scale(.8);",
+      }, {
+        path: "fox3.gif",
+        style: "transform: scale(1.3);",
+      }, {
+        path: "fox4.gif",
+      },
+    ],
+  },
+  {
+    name: "frog",
+    images: [
+      {
+        path: "frog.gif",
+        style: "transform: scale(3);",
+      }, {
+        path: "frog2.gif",
+      }, {
+        path: "frog3.gif",
+        style: "transform: scale(1.3);",
+      }, {
+        path: "frog4.gif",
+        style: "transform: scale(2);",
+      },
+    ],
+  },
+  {
+    name: "giraffe",
+    images: [
+      {
+        path: "giraffe.gif",
+        style: "transform: scale(1.5);",
+      },
+    ],
+  },
+  {
+    name: "hamster",
+    images: [
+      {
+        path: "hamster.gif",
+      }, {
+        path: "hamster3.webp",
+      }, {
+        path: "hamster4.gif",
+      }, {
+        path: "hamster6.gif",
+        style: "transform: scale(1.4);",
+      },
+    ],
+  },
+  {
+    name: "hippo",
+    images: [
+      {
+        path: "hippo.gif",
+        style: "transform: scale(1.3);",
+      },
+    ],
+  },
+  {
+    name: "lion",
+    images: [
+      {
+        path: "lion.gif",
+        style: "transform: scale(1.3);",
+      }, {
+        path: "lion2.gif",
+        style: "transform: scale(1.5);",
+      }, {
+        path: "lion3.gif",
+      },
+    ],
+  },
+  {
+    name: "mouse",
+    images: [
+      {
+        path: "mouse.gif",
+        style: "transform: scale(1.1);",
+      },
+    ],
+  },
+  {
+    name: "panda",
+    images: [
+      {
+        path: "panda.gif",
+      },
+    ],
+  },
+  {
+    name: "penguin",
+    images: [
+      {
+        path: "penguin.webp",
+        style: "transform: scale(3) translateX(-10%);",
+      }, {
+        path: "penguin2.gif",
+      }, {
+        path: "penguin3.gif",
+      },
+    ],
+  },
+  {
+    name: "pig",
+    images: [
+      {
+        path: "pig2.webp",
+        style: "transform: scale(1.3);",
+      }, {
+        path: "pig3.gif",
+        style: "transform: scale(.9);",
+      }, {
+        path: "pig4.gif",
+        style: "transform: scale(.8);",
+      },
+    ],
+  },
+  {
+    name: "rabbit",
+    images: [
+      {
+        path: "rabbit.gif",
+        style: "transform: scale(1.2);",
+      }, {
+        path: "rabbit2.gif",
+      },
+    ],
+  },
+  {
+    name: "reindeer",
+    images: [
+      {
+        path: "reindeer.gif",
+        style: "transform: scale(1.25);",
+      }, {
+        path: "reindeer2.gif",
+        style: "transform: scale(.8);",
+      },
+    ],
+  },
+  {
+    name: "shark",
+    images: [
+      {
+        path: "shark.gif",
+        style: "transform: scale(.7);",
+      }, {
+        path: "shark2.gif",
+        style: "transform: scale(2);",
+      },
+    ],
+  },
+  {
+    name: "sheep",
+    images: [
+      {
+        path: "sheep.gif",
+        style: "transform: scale(1.2);",
+      }, {
+        path: "sheep2.gif",
+        style: "transform: scale(1.2);",
+      },
+    ],
+  },
+  {
+    name: "snake",
+    images: [
+      {
+        path: "snake2.gif",
+        style: "transform: scale(1.2);",
+      }, {
+        path: "snake3.gif",
+      },
+    ],
+  },
+  {
+    name: "squirrel",
+    images: [
+      {
+        path: "squirrel.gif",
+        style: "transform: scale(1.5) translateX(-10%);",
+      },
+    ],
+  },
+  {
+    name: "tiger",
+    images: [
+      {
+        path: "tiger.gif",
+      },
+    ],
+  },
+  {
+    name: "unicorn",
+    images: [
+      {
+        path: "unicorn.gif",
+        style: "transform: scale(1.5);",
+      }, {
+        path: "unicorn2.gif",
+        style: "transform: scale(1.5);",
+      },
+    ],
+  },
+  {
+    name: "whale",
+    images: [
+      {
+        path: "whale.gif",
+      }, {
+        path: "whale2.gif",
+        style: "transform: scale(2);",
+      },
+    ],
+  },
+  {
+    name: "giraffe",
+    images: [
+      {
+        path: "giraffe.gif",
+      },
+    ],
+  },
+  {
+    name: "giraffe",
+    images: [
+      {
+        path: "giraffe.gif",
+      },
+    ],
+  },
+  {
+    name: "giraffe",
+    images: [
+      {
+        path: "giraffe.gif",
+      },
+    ],
+  },
+  {
+    name: "giraffe",
+    images: [
+      {
+        path: "giraffe.gif",
+      },
+    ],
+  },
+  {
+    name: "giraffe",
+    images: [
+      {
+        path: "giraffe.gif",
+      },
+    ],
+  },
+  {
+    name: "giraffe",
+    images: [
+      {
+        path: "giraffe.gif",
+      },
+    ],
+  },
+  {
+    name: "farrah",
+    images: [
+      {
+        path: "farrah.gif",
+      },
+    ],
+    large: true,
+  },
 ];
 
 export interface IAnimal {
   name: string;
-  images: string[];
+  images: IAnimalImage[];
   large?: boolean;
+}
+
+export interface IAnimalImage {
+  path: string;
+  style?: string;
 }
--- src/views/Animal.vue
+++ src/views/Animal.vue
@@ -1,17 +1,25 @@
 <template>
   <div class="v-animal">
-    <v-btn class="back-btn" icon to="/">
+    <v-btn class="back-btn"
+           icon
+           to="/">
       <v-icon>mdi-arrow-left</v-icon>
     </v-btn>
-    <v-row
-      class="v-animal-container"
-      align="center"
-      justify="center"
-      no-gutters
-      :style="backgroundStyle"
-    >
-      <div class="dancing-animal-container">
-        <v-img :src="img" class="dancing-animal" />
+    <v-btn class="refresh-btn"
+           icon
+           v-if="imageCount > 1"
+           @click.prevent="newAnimal">
+      <v-icon>mdi-refresh</v-icon>
+    </v-btn>
+    <v-row class="v-animal-container"
+           align="center"
+           justify="center"
+           no-gutters
+           :style="backgroundStyle">
+      <div class="dancing-animal-container" :key="randomImg.path">
+        <v-img :src="img"
+               :style="randomImg.style"
+               class="dancing-animal" />
       </div>
     </v-row>
   </div>
@@ -19,7 +27,7 @@
 
 <script lang="ts">
 import Vue from "vue";
-import { ANIMALS, IAnimal } from "@/constants/animals";
+import { ANIMALS, IAnimal, IAnimalImage } from "@/constants/animals";
 export default Vue.extend({
   props: {
     animalName: {
@@ -31,24 +39,39 @@ export default Vue.extend({
     animals: ANIMALS,
     baseUrl: process.env.BASE_URL || "",
     backgroundStyle: "background-color: #E53935;",
+    selectedAnimal: -1,
   }),
   computed: {
     animal(): IAnimal | undefined {
       return this.animals.find(val => val.name === this.animalName);
     },
+    randomImg(): IAnimalImage {
+      return this.animal && this.animal.images[this.selectedAnimal] || {path: ""};
+    },
+    imageCount(): number {
+      return this.animal && this.animal.images.length || 0;
+    },
     img(): string {
       if (this.animal) {
-        const imgFolder = `${this.baseUrl}/img/animals/${this.animal.name}/`;
-        const imgFile =
-          this.animal.images[
-            Math.floor(Math.random() * this.animal.images.length)
-          ] || "";
-        return imgFolder + imgFile;
+        const imgFolder = `${this.baseUrl}img/animals/${this.animal.name}/`;
+        return imgFolder + this.randomImg.path;
       }
       return "";
     },
   },
+  methods: {
+    newAnimal() {
+      let newAnimal = Math.floor(Math.random() * this.imageCount);
+      let tries = 0;
+      while (newAnimal === this.selectedAnimal && tries < 10) {
+        newAnimal = Math.floor(Math.random() * this.imageCount);
+        tries++;
+      };
+      this.selectedAnimal = newAnimal || 0;
+    },
+  },
   mounted() {
+    this.newAnimal();
     setTimeout(() => {
       this.backgroundStyle = "";
     }, 0);
@@ -68,6 +91,11 @@ export default Vue.extend({
     position: fixed;
     top: 10px;
     left: 10px;
+  }
+  .refresh-btn {
+    position: fixed;
+    top: 10px;
+    right: 10px;
   }
   .dancing-animal-container {
     width: 10%;
